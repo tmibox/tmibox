56947b84347caaca3659789172214dc06bc6bb83
Jaeyeol Lee <rijgndqw012@gmail.com>
2021-12-29 11:04:34 +0900

    feat: Redirect to `CardPage` when click on `TmiCardListEntry` component

--- lib/tmibox_web/components/tmi_card_list_entry.ex
+++ lib/tmibox_web/components/tmi_card_list_entry.ex
@@ -3,34 +3,40 @@ defmodule TmiboxWeb.Components.TmiCardListEntry do
   메인 화면에서 보여지는 리스트 뷰의 TmiCard 컴포넌트
   """
   use Surface.Component
+  alias Surface.Components.Link
+  alias TmiboxWeb.Router.Helpers, as: Routes
 
   @doc "Card"
   prop card, :map
 
 
   def render(assigns) do
+    card_id = assigns.card |> Map.get(:id, 0)
+
     ~F"""
-    <div id={"tmi-card-#{@card |> Map.get(:id, 0)}"} class="tmi-card-list-entry card shadow-lg bordered">
-      <div class="card-body">
-        <div class="card-body-wrapper">
-          <h2 class="card-title">{@card.title}</h2>
-          <p>{@card.content}</p>
-        </div>
-        <div class="card-actions justify-end">
-          <div class="like-action flex items-center justify-center">
-            <button class="btn btn-circle btn-outline btn-accent">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-              </svg>
-            </button>
+    <Link id={"tmi-card-#{card_id}"} to={Routes.live_path(TmiboxWeb.Endpoint, TmiboxWeb.Pages.CardPage, card_id)}>
+      <div class="tmi-card-list-entry card shadow-lg bordered">
+        <div class="card-body">
+          <div class="card-body-wrapper">
+            <h2 class="card-title">{@card.title}</h2>
+            <p>{@card.content}</p>
           </div>
-          <div class="bookmark-action flex item-center justify-center">
-          </div>
-          <div class="share-action flex item-center justify-center">
+          <div class="card-actions justify-end">
+            <div class="like-action flex items-center justify-center">
+              <button class="btn btn-circle btn-outline btn-accent">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                </svg>
+              </button>
+            </div>
+            <div class="bookmark-action flex item-center justify-center">
+            </div>
+            <div class="share-action flex item-center justify-center">
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
     """
   end
 end
--- test/tmibox_web/live/cards_page_test.exs
+++ test/tmibox_web/live/cards_page_test.exs
@@ -8,25 +8,61 @@ defmodule TmiboxWeb.CardsPageTest do
 
   import Tmibox.Factory
 
-  test "connected mount", %{conn: conn} do
-    card1 = insert(:card, title: "Hello", content: "World")
-    card2 = insert(:card, title: "Hello1", content: "World2")
-    card3 = insert(:card, title: "Hello2", content: "World3")
+  alias TmiboxWeb.Router.Helpers, as: Routes
 
-    conn = get(conn, "/")
+  describe "About page loading" do
+    test "TmiCard의 갯수만큼 성공적으로 렌더링이 된다.", %{conn: conn} do
+      card1 = insert(:card, title: "Hello", content: "World")
+      card2 = insert(:card, title: "Hello1", content: "World2")
+      card3 = insert(:card, title: "Hello2", content: "World3")
 
-    {:ok, view, _html} = live(conn)
+      conn = get(conn, "/")
 
-    assert view
-      |> element("#tmi-card-#{card1.id}")
-      |> has_element?()
+      {:ok, view, _html} = live(conn)
 
-    assert view
-      |> element("#tmi-card-#{card2.id}")
-      |> has_element?()
+      assert view
+        |> element("#tmi-card-#{card1.id}")
+        |> has_element?()
 
-    assert view
-      |> element("#tmi-card-#{card3.id}")
-      |> has_element?()
+      assert view
+        |> element("#tmi-card-#{card2.id}")
+        |> has_element?()
+
+      assert view
+        |> element("#tmi-card-#{card3.id}")
+        |> has_element?()
+    end
+  end
+
+  describe "About redirection" do
+    test "각각의 TmiCard에 대한 상세페이지로 리다이렉션이 된다.", %{conn: conn} do
+      card1 = insert(:card, title: "Hello", content: "World")
+      card2 = insert(:card, title: "Hello1", content: "World2")
+      card3 = insert(:card, title: "Hello2", content: "World3")
+
+      conn = get(conn, "/")
+      {:ok, view, _html} = live(conn)
+
+      view
+        |> element("#tmi-card-#{card1.id}")
+        |> render_click
+      assert_redirect(view, Routes.live_path(TmiboxWeb.Endpoint, TmiboxWeb.Pages.CardPage, card1.id))
+
+      conn = get(conn, "/")
+      {:ok, view, _html} = live(conn)
+
+      view
+        |> element("#tmi-card-#{card2.id}")
+        |> render_click
+      assert_redirect(view, Routes.live_path(TmiboxWeb.Endpoint, TmiboxWeb.Pages.CardPage, card2.id))
+
+      conn = get(conn, "/")
+      {:ok, view, _html} = live(conn)
+
+      view
+        |> element("#tmi-card-#{card3.id}")
+        |> render_click
+      assert_redirect(view, Routes.live_path(TmiboxWeb.Endpoint, TmiboxWeb.Pages.CardPage, card3.id))
+    end
   end
 end
