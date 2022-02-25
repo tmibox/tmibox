4d05ff640290f79e426e56b9432fca33d3965614
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-02-25 21:51:09 +0900

    chore: Setup Credo (Linter for Elixir)

---
+++ .credo.exs
@@ -0,0 +1,23 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: ["lib/", "test/"],
+        excluded: [~r"/_build/", ~r"/deps/"]
+      },
+      check_for_updates: true,
+      strict: false,
+      color: true,
+      checks: [
+        {Credo.Check.Consistency.ExceptionNames},
+        {Credo.Check.Consistency.LineEndings},
+        {Credo.Check.Consistency.MultiAliasImportRequireUse},
+        {Credo.Check.Consistency.ParameterPatternMatching},
+        {Credo.Check.Consistency.SpaceAroundOperators},
+        {Credo.Check.Consistency.SpaceInParentheses},
+        {Credo.Check.Consistency.TabsOrSpaces},
+      ]
+    }
+  ]
+}
