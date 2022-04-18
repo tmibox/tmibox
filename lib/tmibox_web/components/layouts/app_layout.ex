aedc1de4340ded824fd563fe5a283331669d99c1
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-04-19 01:14:15 +0900

    ui: Redesign navbar

--- lib/tmibox_web/components/layouts/app_layout.ex
+++ lib/tmibox_web/components/layouts/app_layout.ex
@@ -17,21 +17,56 @@ defmodule TmiboxWeb.Components.Layouts.AppLayout do
   def render(assigns) do
     ~F"""
     <header>
-      <section class="container">
-        <nav>
-          <ul>
-            <li><a href="https://hexdocs.pm/phoenix/overview.html">Get Started</a></li>
-            {#if function_exported?(Routes, :live_dashboard_path, 2) }
-              <li>{ link "LiveDashboard", to: Routes.live_dashboard_path(Endpoint, :home) }</li>
-            {/if}
-          </ul>
-        </nav>
-        <a href="https://phoenixframework.org/" class="phx-logo">
-          <img src={Routes.static_path(Endpoint, "/images/phoenix.png")} alt="Phoenix Framework Logo"/>
-        </a>
+      <section class="container max-w-screen-md mx-auto">
+        <div class="navbar bg-base-100 justify-between">
+          <div class="flex-1">
+            <a
+              class="btn btn-ghost normal-case text-xl"
+              href="/"
+            >
+              TMI Box
+            </a>
+          </div>
+          <div class="flex-none">
+            <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle">
+                <div class="indicator">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                  <span class="badge badge-sm indicator-item">8</span>
+                </div>
+              </label>
+              <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                <div class="card-body">
+                  <span class="font-bold text-lg">8 Items</span>
+                  <span class="text-info">Subtotal: $999</span>
+                  <div class="card-actions">
+                    <button class="btn btn-primary btn-block">View cart</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                  <img src="https://api.lorem.space/image/face?hash=33791" />
+                </div>
+              </label>
+              <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <a class="justify-between">
+                    Profile
+                    <span class="badge">New</span>
+                  </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li><a>Logout</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     </header>
-    <main class="container">
+    <main class="container max-w-screen-md mx-auto">
       <p class="alert alert-info" role="alert"
         phx-click="lv:clear-flash"
         phx-value-key="info">{ live_flash(@flash, :info) }</p>
